8f8bab7a71f7e3417b3e5a450f6987362ba1f994
PoRATL <Like107.Lol@yandex.ru>
2025-05-07 04:10:51 +0300

    Fix 4

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="devops-lab5",
     version="0.1.0",
-    packages=find_packages(),  # Автоматически найдет все пакеты (папки с __init__.py)
-    install_requires=[         # Укажите зависимости проекта
+    packages=find_packages(),
+    install_requires=[
         "fastapi",
         "uvicorn",
+        "pydantic-settings",
+        "pydantic>=2.0.0",
         "httpx",
-        "pytest"
+        "sqlalchemy",
+        "python-multipart"
     ],
 )
